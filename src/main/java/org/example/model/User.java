e44d83996f06cf9d5493980c030f32b066ec58bc
1nullptr <129955496+1nullptr@users.noreply.github.com>
2023-04-20 15:00:57 -0300

    Added UserRepo class

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public class User {
     private String m_Name;
+    private String m_Email;
     private int m_Age;
     private List<String> m_PreferredGenres;
 
-    public User(String name, int age) {
+    public User(String name, int age, String email) {
         this.m_Name = name;
         this.m_Age = age;
+        this.m_Email = email;
         this.m_PreferredGenres = new ArrayList<>();
     }
 
@@ -36,5 +38,13 @@ public class User {
 
     public void setPreferredGenres(List<String> preferredGenres) {
         this.m_PreferredGenres = preferredGenres;
+    }
+
+    public String getEmail() {
+        return m_Email;
+    }
+
+    public void setEmail(String email) {
+        this.m_Email = email;
     }
 }
---
+++ src/main/java/org/example/repo/UserRepo.java
@@ -0,0 +1,51 @@
+package org.example.repo;
+
+import org.example.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepo {
+    public void addUser(User user) {
+        try{
+            //Connection connection = // TODO: Get connection from ConnectionPool
+            String sql = "INSERT INTO user (name, age, email) VALUES (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, user.getName());
+            statement.setInt(2, user.getAge());
+            statement.setString(3, user.getEmail());
+            statement.executeUpdate();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        try{
+            //Connection connection = // TODO: Get connection from ConnectionPool
+            String sql = "SELECT * FROM user";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String email = resultSet.getString("email");
+                User user = new User(name, age, email);
+                users.add(user);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+}
