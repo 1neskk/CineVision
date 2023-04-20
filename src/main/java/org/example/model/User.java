b9d52f284a350641ea4b11b7be3de95c7fc17301
1nullptr <129955496+1nullptr@users.noreply.github.com>
2023-04-20 14:34:12 -0300

    Added User class

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,5 +1,40 @@
 package org.example.model;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+    private String m_Name;
+    private int m_Age;
+    private List<String> m_PreferedGenres;
+
+    public User(String name, int age) {
+        this.m_Name = name;
+        this.m_Age = age;
+        this.m_PreferedGenres = new ArrayList<>();
+    }
+
+    public String getName() {
+        return m_Name;
+    }
+
+    public void setName(String name) {
+        this.m_Name = name;
+    }
+
+    public int getAge() {
+        return m_Age;
+    }
+
+    public void setAge(int age) {
+        this.m_Age = age;
+    }
+
+    public List<String> getPreferedGenres() {
+        return m_PreferedGenres;
+    }
+
+    public void setPreferedGenres(List<String> preferedGenres) {
+        this.m_PreferedGenres = preferedGenres;
+    }
 }
