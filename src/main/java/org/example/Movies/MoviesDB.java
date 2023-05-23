b409204dcea653d144c3e4c95cdcd4602f282bfd
1darkz <daniel444almeida@outlook.com>
2023-05-23 11:42:50 -0300

    Created Movie class and added a class to register the movies to the database.

---
+++ src/main/java/org/example/Movies/MoviesDB.java
@@ -0,0 +1,31 @@
+package org.example.Movies;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MoviesDB {
+    private static final String DB_URL = "jdbc:mysql://%s:%s/%s?useTimezone=true&serverTimezone=America/Sao_Paulo";
+    private static final String HOST = "localhost";
+    private static final String PORT = "3306";
+    private static final String DATABASE = "cinevision";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "anima123";
+
+    public void registerMovie(Movie movie) {
+        try (Connection connection = DriverManager.getConnection(String.format(DB_URL, HOST, PORT, DATABASE), DB_USER, DB_PASSWORD)) {
+            String sql = "INSERT INTO movies (title, year, director, genre, rating) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, movie.GetTitle());
+            statement.setInt(2, movie.GetReleaseYear());
+            statement.setString(3, movie.GetDirector());
+            statement.setString(4, movie.GetGenre());
+            statement.setArray(5, (java.sql.Array) movie.GetRating());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
